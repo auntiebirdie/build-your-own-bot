e9baa09d55e38618f157ff8f05f25805fb227de9
seppukawaii <seppukawaii@gmail.com>
2024-01-27 18:05:04 -0500

    Birthday (Part 1)

---
+++ 04-birthday/interactions/check.js
@@ -0,0 +1,23 @@
+const JSONdb = require('simple-json-db')
+
+module.exports = (interaction) => {
+  const targetUser = interaction.options.getUser('user')
+  const db = new JSONdb(`db/${targetUser.id}.json`)
+  const birthdayData = db.get('birthday')
+
+  if (birthdayData) {
+    const birthday = new Date()
+    birthday.setMonth(birthdayData.month)
+    birthday.setDate(birthdayData.day)
+
+    interaction.reply({
+      content: `<@${targetUser.id}>'s birthday is ${birthday.toLocaleString('default', { month: 'long', day: 'numeric' })}.`,
+      ephemeral: true
+    })
+  } else {
+    interaction.reply({
+      content: `<@${targetUser.id}> hasn't set their birthday yet.`,
+      ephemeral: true
+    })
+  }
+}
